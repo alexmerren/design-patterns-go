60656c1a41a6c86d35aff75058c49f1854e544fe
alex-merren <alex.merren@hpe.com>
2022-03-22 16:29:40 +0000

    *

--- structural/proxy/proxy.go
+++ structural/proxy/proxy.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 const (
 	overdraftLimit   = -100_00
 	suspiciousAmount = 10_000_00
@@ -18,19 +16,23 @@ func NewProxyBankAccount(name string, balancePence int) *ProxyBankAccount {
 }
 
 func (b *ProxyBankAccount) Deposit(amountPence int) int {
+	// We should probably tell someone that a suspicious amount of money is being deposited
 	if amountPence >= suspiciousAmount {
-		fmt.Println("Woah. That's a lot of money")
 	}
 	return b.account.Deposit(amountPence)
 }
 
 func (b *ProxyBankAccount) Withdraw(amountPence int) int {
+	// We should probably tell someone that a suspicious amount of money is being withdrawn
+	if amountPence >= suspiciousAmount {
+	}
+
+	// In reality, this would probably be done with command pattern so
+	// undo() can be called, but since we know that the withdraw() and
+	// deposit() functions are just adding and subtracting from balance,
+	// this is okay.
 	balance := b.account.Withdraw(amountPence)
 	if balance <= overdraftLimit {
-		// In reality, this would probably be done with command pattern so
-		// undo() can be called, but since we know that the withdraw() and
-		// deposit() functions are just adding and subtracting from balance,
-		// this is okay.
 		b.account.Deposit(amountPence)
 		return 0
 	}
